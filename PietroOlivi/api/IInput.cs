17b5185ef06e7cb1ddb5672a4fce02aee292bb14
pietroolivi <pietroolivi@icloud.com>
2023-04-17 11:17:13 +0200

    Removed Namespace

--- PietroOlivi/api/IInput.cs
+++ PietroOlivi/api/IInput.cs
@@ -1,25 +1,22 @@
 using System;
 
-namespace IInput
-{ 
-    /*
-     * Interface to model the inputs.
-     */
-    public interface IInput : ICloneable 
-    {
-        bool MoveUp { get; set; }
-        bool MoveDown { get; set; }
-        bool MoveRight { get; set; }
-        bool MoveLeft { get; set; }
-        bool Jump { get; set; }
-        bool Forward { get; set; }
-        bool Backwards { get; set; }
-        Int32? NumberPressed { get; set; }
+/*
+ * Interface to model the inputs.
+ */
+public interface IInput : ICloneable 
+{
+    bool MoveUp { get; set; }
+    bool MoveDown { get; set; }
+    bool MoveRight { get; set; }
+    bool MoveLeft { get; set; }
+    bool Jump { get; set; }
+    bool Forward { get; set; }
+    bool Backwards { get; set; }
+    Int32? NumberPressed { get; set; }
 
-        /*
-         * Method to reset all inputs to false.
-         */
-        void Reset();
-    }
+    /*
+        * Method to reset all inputs to false.
+        */
+    void Reset();
 }
         
