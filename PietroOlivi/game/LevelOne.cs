1d8c04ab4f4f238f6a7ab0ac872f44b5f9f06fb1
pietroolivi <pietroolivi@icloud.com>
2023-04-19 22:59:38 +0200

    Created LevelOne.cs class

---
+++ PietroOlivi/game/LevelOne.cs
@@ -0,0 +1,42 @@
+using OOP22_mtsk_game_csharp.LeonardoTassinari.game;
+using OOP22_mtsk_game_csharp.PietroOlivi.api;
+
+namespace OOP22_mtsk_game_csharp.PietroOlivi.game
+{
+    /*
+     * Class that contains values related to 
+     * the first level of the whac-a-mole game.
+     */
+    internal class LevelOne : ILevel
+    {
+        private static readonly int MAX_OBJS_OUT_AT_ONCE = 1;
+        private static readonly double VECTOR_ORDINATE = -20;
+        private static readonly long MIN_WAIT_TO_SPAWN = 5_000L;
+        private static readonly long MAX_WAIT_TO_SPAWN = 15_000L;
+        public static readonly long STATIONARY_TIME = 5000L;
+        
+        public LevelOne()
+        {
+        }
+
+        public int GetMaxObjsSimultaneouslyOut() 
+        {
+            return LevelOne.MAX_OBJS_OUT_AT_ONCE;
+        }
+
+        public Vector2D GetObjSpeed() 
+        {
+            return new Vector2D(0, LevelOne.VECTOR_ORDINATE);
+        }
+
+        public TimeInterval GetSpawnWaitingTime() 
+        {
+            return new TimeInterval(LevelOne.MIN_WAIT_TO_SPAWN, LevelOne.MAX_WAIT_TO_SPAWN);
+        }
+
+        public long GetHalfwayTime() 
+        {
+            return LevelOne.STATIONARY_TIME;
+        }
+    }
+}
