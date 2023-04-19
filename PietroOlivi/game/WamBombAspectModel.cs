9055747ede7ee422e014f9aa4e1214a4c798528e
pietroolivi <pietroolivi@icloud.com>
2023-04-19 20:31:15 +0200

    Created WamBombAspectModel.cs class

---
+++ PietroOlivi/game/WamBombAspectModel.cs
@@ -0,0 +1,32 @@
+using OOP22_mtsk_game_csharp.LorenzoDalmonte.api;
+
+namespace OOP22_mtsk_game_csharp.PietroOlivi.game 
+{
+    /*
+     * I order to draw the Bomb only if it has actually 
+     * entered the game, i.e. if it has already been 
+     * spawned, in which case I distinguish the object's 
+     * appearance based on whether it has been hit or not.
+     */
+    public class WamBombAspectModel : IAspectModel
+    {
+        public void Update(GameObject obj, IDrawings drawing)
+        {        
+            if (obj is WamObject) 
+            {
+                Status status = ((WamObject) obj).GetStatus();
+                if (!status.Equals(Status.WAITING)) 
+                {
+                    if (status.Equals(Status.HIT)) 
+                    {
+                        drawing.DrawWamBomb(obj, true);
+                    } 
+                    else 
+                    {
+                        drawing.DrawWamBomb(obj, false);
+                    }
+                }
+            }
+        }
+    }
+}
