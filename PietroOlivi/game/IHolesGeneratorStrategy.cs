0dc4e2f57adc652120928b44baea8673452aa910
pietroolivi <pietroolivi@icloud.com>
2023-04-19 22:58:47 +0200

    Created IHolesGeneratorStrategy.cs interface

---
+++ PietroOlivi/game/IHolesGeneratorStrategy.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace OOP22_mtsk_game_csharp.PietroOlivi.game
+{
+    /*
+     * Interface for the burrow placement and creation algorithm.
+     */
+    internal interface IHolesGeneratorStrategy
+    {
+        /*
+         * Method responsible for placing and making holes.
+         */
+        IList<WamObject> generate(int numHoles);
+    }
+}
