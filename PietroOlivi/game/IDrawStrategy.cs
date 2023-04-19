55e8e679e3beb83ba79a3094f8726d4207a1bbbc
pietroolivi <pietroolivi@icloud.com>
2023-04-19 20:27:56 +0200

    Created IDrawStrategy.cs interface

---
+++ PietroOlivi/game/IDrawStrategy.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using OOP22_mtsk_game_csharp.LorenzoDalmonte.api;
+
+namespace OOP22_mtsk_game_csharp.PietroOlivi.game 
+{
+    /*
+     * Interface used as a strategy pattern, containing the 
+     * interchangeable algorithm that creates moles/bombs in random 
+     * number and positions (also assigning them an arbitrary appearance 
+     * time) within the limits set by the current level of difficulty.
+     */
+    public interface IDrawStrategy 
+    {
+
+        /*
+         * Method for randomly drawing moles and/or bombs.
+         */
+        ISet<GameObject> Draw(ILevel currentLevel, long currentTime);
+    }
+}
