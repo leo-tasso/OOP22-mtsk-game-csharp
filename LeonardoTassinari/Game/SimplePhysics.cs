c9922b553326b5f7053aa1d5b3b81317dba91ab4
pietroolivi <pietroolivi@icloud.com>
2023-04-19 20:22:40 +0200

    Changed from internal to public

--- LeonardoTassinari/Game/SimplePhysics.cs
+++ LeonardoTassinari/Game/SimplePhysics.cs
@@ -2,7 +2,7 @@
 
 namespace OOP22_mtsk_game_csharp.LeonardoTassinari.game
 {
-    internal class SimplePhysics : IPhysicsModel
+    public class SimplePhysics : IPhysicsModel
     {
         private static readonly double SPEED_COEFF = 0.01;
         public void Update(long dt, GameObject obj, IMinigame miniGame)
