1cddf5cbdc6cedc9b4ea9081af30f0652f24800c
Lorenzo Dalmonte <lorenzo.dalmonte4@studio.unibo.it>
2023-04-20 18:16:25 +0200

    Added DatTriangle

---
+++ LorenzoDalmonte/game/DatTriangle.cs
@@ -0,0 +1,18 @@
+using System;
+using OOP22_mtsk_game_csharp.LeonardoTassinari.game;
+using OOP22_mtsk_game_csharp.LorenzoDalmonte.api;
+using OOP22_mtsk_game_csharp.PietroOlivi.game;
+
+namespace OOP22_mtsk_game_csharp.LorenzoDalmonte.game
+{
+    public class DatTriangle : GameObject
+    {
+        public DatTriangle(Point2D coor, Vector2D vel, int side) : base(coor, vel)
+        {
+            this.Input = new NullInput();
+            this.Physics = new SimplePhysics();
+            this.Aspect = new DatTriangleAspectModel(side, coor.X > 0);
+            this.HitBox = new RectangleHitBoxModel(side * Math.Sqrt(3) / 2, side);
+        }
+    }
+}
