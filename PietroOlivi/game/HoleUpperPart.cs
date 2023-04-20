7e45a6ac951b56b3e00f827e827db4db27179724
pietroolivi <pietroolivi@icloud.com>
2023-04-20 09:45:10 +0200

    Created HoleUpperPart.cs class

---
+++ PietroOlivi/game/HoleUpperPart.cs
@@ -0,0 +1,36 @@
+using OOP22_mtsk_game_csharp.LeonardoTassinari.game;
+using OOP22_mtsk_game_csharp.LorenzoDalmonte.api;
+
+namespace OOP22_mtsk_game_csharp.PietroOlivi.game
+{
+    /*
+     * Class that represents the upper part of the hole or rather 
+     * the semicircle that gives a 3D effect, above which moles and 
+     * bombs will have to be displayed. Having a different appearance 
+     * than the bottom, and having to keep it in a different 
+     * position on the GameObject list, it has a class of its own.
+     */
+    public class HoleUpperPart : HolePart
+    {
+        /*
+         * Simple constructor aimed at initializing hole's upper part
+         */
+        public HoleUpperPart(Point2D coor,
+            long appearanceTime, 
+            ILevel currentLevel, 
+            int holeNumber, 
+            IPhysicsModel physicsModel,
+            IAspectModel aspectModel, 
+            IInputModel inputModel) : base(
+                coor, 
+                appearanceTime, 
+                currentLevel, 
+                holeNumber, 
+                physicsModel, 
+                aspectModel, 
+                inputModel
+            )
+        {
+        }
+    }
+}
